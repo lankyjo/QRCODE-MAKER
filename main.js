5d8355057edee56540d90403e51b9dba2089b9bf
lankyjo <ikejijoshua69@gmail.com>
2024-07-18 22:52:50 +0100

    added loader

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 let qrimage = document.querySelector('.qr img');
-let content = document.getElementById('content')
-let btn = document.getElementById('btnCreate')
+let loader = document.querySelector('.loader');
+let content = document.getElementById('content');
+let btn = document.getElementById('btnCreate');
 
-btn.onclick = () =>{
-    if(content.value === '') return;
+btn.onclick = () => {
+    if (content.value === '') return;
 
-    // let linkQR = 'https://api.qrserver.com/v1/create-qr-code/'
-    qrimage.src = `https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=${content.value}`
-    console.log(qrimage.src)
+    // Show loader
+    loader.style.display = 'block';
+
+    // Hide the QR image while loading
+    qrimage.style.display = 'none';
+
+    qrimage.src = `https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=${content.value}`;
+    console.log(qrimage.src);
+
+    qrimage.onload = () => {
+        // Hide loader and show QR image
+        loader.style.display = 'none';
+        qrimage.style.display = 'block';
+    };
+
     content.value = '';
-}
-
+};
